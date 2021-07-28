462092f07cf3c6488623d0d88f3f09361b842b35
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-07-28 11:44:21 -0400

    added user choices

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import  { useState }  from "react";
+
 const App = () => {
+  const [userChoice, setuserChoice] = useState('null')
+  const choices = ['rock', 'paper', 'scissors']
+
+  const handleClick = (value) => {
+    setuserChoice(value)
+  }
   return (
     <div>
+      <h1>user choice is:{userChoice}</h1>
+      <h1>computer choice is:</h1>
+      {/* <button onClick={() => handleClick('rock') }>Rock</button>
+      <button onClick={() => handleClick('paper')}>Paper</button>
+      <button onClick={() => handleClick('scissors')}>Scissor</button> */}
+      
+      {choices.map(choice => <button onClick={() => handleClick(choice)}>{choice}</button> )}
+
     </div>
   );
 }
