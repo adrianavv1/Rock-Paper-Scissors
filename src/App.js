8bc08703c01ee65c479d3c3160e41aaadb1bb41f
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-07-28 13:53:12 -0400

    added results

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import { useState } from "react";
+import { check } from "yargs";
 
 const App = () => {
   const [userChoice, setuserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
   const choices = ["rock", "paper", "scissors"];
 
   const handleClick = (value) => {
-    setuserChoice(value);
+    setuserChoice(value)
+    generateComputerChoice()
+    checkResult();
   };
 
   const generateComputerChoice = () => {
-    const randomChoice = choices[Math.floorMath.random() * choices.length]
-  }
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+  };
+
+  const checkResult = () => {
+    switch (userChoice + computerChoice) {
+      case 'scissorspaper':
+      case 'rockscissors':
+      case 'paperrock':
+        setResult('YOU WIN!');
+        break;
+      case 'paperscissors':
+      case 'rockpaper':
+        setResult('YOU LOSE!');
+        break;
+      case 'rockrock':
+      case 'paper':
+      case 'scissorsscissors':
+        setResult('ITS A DRAW');
+        break;
+    }
+  };
   return (
     <div>
       <h1>user choice is:{userChoice}</h1>
-      <h1>computer choice is:</h1>
-      {/* <button onClick={() => handleClick('rock') }>Rock</button>
-      <button onClick={() => handleClick('paper')}>Paper</button>
-      <button onClick={() => handleClick('scissors')}>Scissor</button> */}
+      <h1>computer choice is:{computerChoice}</h1>
 
       {choices.map((choice, index) => (
         <button key={index} onClick={() => handleClick(choice)}>
